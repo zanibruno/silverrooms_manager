3829280234635ffdad8ba5e1432200a47fe5cee4
zanibruno <corporate.zanin@gmail.com>
2020-02-25 16:36:19 +0000

    created scope for rated = true

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -3,7 +3,11 @@ class Rating < ApplicationRecord
     belongs_to :tenant
 
     validates :rating , presence: true
-    valdates :comment, presence: true
+    validates :comment, presence: true
     validates :tenant_id, presence: true
-    validates :property_id, presence: true 
+    validates :property_id, presence: true
+
+    scope :rated, -> { where(rating: true) }
+
+
 end
